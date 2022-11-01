b649a05e79c75aee71da2dc8e84fc1b47c251563
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-01 23:59:38 +0300

    After Day 10 , Homework 4
    After Day 10 , Homework 4

---
+++ AfterDay10_ReCapProject_Homework4/Business/Concrete/CarManager.cs
@@ -0,0 +1,89 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public IResult Add(Car car)
+        {
+            if(car.Description.Length>=2)
+            {
+                if (car.DailyPrice > 0)
+                {
+                    _carDal.Add(car);
+                    return new SuccessResult(Messages.SaveSuccessful);
+                }                    
+                else
+                {
+                    Console.WriteLine("Günlük Kiralama Bedeli O TL den fazla olmalıdır");
+                    Console.WriteLine(Messages.SaveFailed);
+                    return new ErrorResult(Messages.SaveFailed);
+                }
+                    
+            }
+            else
+            {
+                Console.WriteLine(Messages.SaveFailed);
+                Console.WriteLine("Araba açıklaması 2 karakterden küçük olamaz");
+                return new ErrorResult(Messages.SaveFailed);
+            }            
+        }
+
+        public IResult Delete(Car car)
+        {
+            _carDal.Delete(car);
+            return new SuccessResult(Messages.DeleteSuccessful);
+        }
+
+        public IDataResult<List<Car>> GetAll()
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.ListedSuccessful);
+        }
+
+        public IDataResult<Car> GetById(int CarId)
+        {
+            return new SuccessDataResult<Car>(_carDal.GetById(p=>p.CarId==CarId),Messages.ListedSuccessful);
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(),Messages.ListedSuccessful);
+        }
+
+        public IDataResult<List<Car>> GetCarsByBrandId(int BrandId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c=>c.BrandId==BrandId),Messages.ListedSuccessful);
+            
+        }
+        public IDataResult<List<Car>> GetCarsByColorId(int ColorId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == ColorId),Messages.ListedSuccessful);
+
+        }
+        
+
+        public IResult Update(Car car)
+        {
+            _carDal.Update(car);
+            return new SuccessResult(Messages.UpdateSuccessful);
+        }
+    }
+}
---
+++ AfterDay10_ReCapProject_Homework4/DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -0,0 +1,18 @@
+﻿using Core.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfColorDal :EfEntityRepositoryBase<Color,CarRentContext>, IColorDal
+    {
+      
+    }
+}
