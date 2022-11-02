6e1aed0e0496718e844b34c91f475f9d78b44674
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-02 15:08:38 +0300

    After Day 11
    After Day 11

---
+++ AfterDay11_ReCapProject/Business/Concrete/UserManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+        public UserManager(IUserDal userDal)
+        {
+            _userDal=userDal;
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult(Messages.SaveSuccessful);
+        }
+
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult(Messages.DeleteSuccessful);
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            if(_userDal.GetAll().Count==0)
+            {
+                return new ErrorDataResult<List<User>>(_userDal.GetAll(),Messages.ListedFailed);
+            }
+            return new SuccessDataResult<List<User>>(_userDal.GetAll(), Messages.ListedSuccessful);
+        }
+
+        public IDataResult<User> GetById(int id)
+        {
+            return new SuccessDataResult<User>(_userDal.GetById(u=>u.Id==id),Messages.ListedSuccessful);
+        }
+
+        public IResult Update(User user)
+        {
+            _userDal.Update(user);
+            return new SuccessResult(Messages.UpdateSuccessful);
+        }
+    }
+}
---
+++ AfterDay11_ReCapProject/Entities/DTOs/CustomerDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class CustomerDetailDto:IDto
+    {
+        public int Id { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string CompanyName { get; set; }
+
+    }
+}
