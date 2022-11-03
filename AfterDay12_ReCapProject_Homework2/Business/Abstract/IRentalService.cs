ab9c49f62562679de9350bf6fd5cc85576dd7685
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-03 20:39:22 +0300

    After Day 12 , Homework 2
    After Day 12 , Homework 2

---
+++ AfterDay12_ReCapProject_Homework2/Business/Abstract/IRentalService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IRentalService
+    {
+        IResult Add(Rental rental);
+        IResult Update(Rental rental);
+        IResult Delete(Rental rental);
+        IDataResult<List<Rental>> GetAll();
+        IDataResult<Rental> GetById(int id);
+        IDataResult<List<RentalDetailDto>> GetRentalDetails();
+    }
+}
---
+++ AfterDay12_ReCapProject_Homework2/DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,37 @@
+﻿using Core.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarRentContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (CarRentContext context = new CarRentContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands on c.BrandId equals b.BrandId
+                             join cl in context.Colors on c.ColorId equals cl.ColorId
+                             select new CarDetailDto
+                             {
+                                 CarName = c.Description,
+                                 BrandName = b.BrandName,
+                                 ColorName = cl.ColorName,
+                                 DailyPrice = c.DailyPrice
+                             };
+                return result.ToList();
+                             
+                             
+            }
+        }
+    }
+}
