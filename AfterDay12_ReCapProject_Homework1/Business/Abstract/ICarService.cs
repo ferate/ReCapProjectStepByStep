cf9ba99aa0f82c528d2476b2fb6d1b7fbfc4b068
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-03 19:02:13 +0300

    After Day 12 , Homework 1
    After Day 12 , Homework 1

---
+++ AfterDay12_ReCapProject_Homework1/Business/Abstract/ICarService.cs
@@ -0,0 +1,31 @@
+﻿using Core.Utilities;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        IDataResult<List<Car>> GetAll();
+
+        IDataResult<List<CarDetailDto>> GetCarDetails();
+
+        IDataResult<List<Car>> GetCarsByBrandId(int BrandId);
+        IDataResult<Car> GetById(int CarId);        
+
+        IResult Add(Car car);
+
+        IResult Update(Car car);
+
+        IResult Delete(Car car);
+
+        
+
+    }
+}
---
+++ AfterDay12_ReCapProject_Homework1/DataAccess/Abstract/CarRentContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public class CarRentContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RentCar;Trusted_Connection=true");
+        }
+
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+    }
+}
