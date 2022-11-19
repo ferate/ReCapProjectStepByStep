94d1e57743f37dc6931c3097ad4f25350d53a962
Ferhat EFİL <32821105+ferate@users.noreply.github.com>
2022-11-19 20:36:09 +0300

    Default Image Test Added
    Default Image Test Added

--- AfterDay14_ReCapProject/WebAPI/Controllers/CarImagesController.cs
+++ AfterDay14_ReCapProject/WebAPI/Controllers/CarImagesController.cs
@@ -55,7 +55,29 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
-       
-       
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {            
+            var result = _carImageService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbycarid")]
+        public IActionResult GetByCarId(int carId)
+        {
+            var result = _carImageService.GetByCarId(carId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
     }
 }
